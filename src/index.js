5d87a117adda4bb6284e277aa9df6b8429004b4f
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-24 15:30:40 -0500

    initialized firebase

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-//webpack firebase application
+import { initializeApp } from 'firebase/app'
 
-console.log('hollo from webpack')
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBB5lvecBSO0-LtVSN4EuYU2AiiebECyAI",
+  authDomain: "fir-dojo-b6120.firebaseapp.com",
+  projectId: "fir-dojo-b6120",
+  storageBucket: "fir-dojo-b6120.appspot.com",
+  messagingSenderId: "712962080563",
+  appId: "1:712962080563:web:65310d6626d44136ef8846"
+}
+
+initializeApp(firebaseConfig)
